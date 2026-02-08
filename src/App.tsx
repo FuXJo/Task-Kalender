dea6e7ab03e519a506c832a9a1f43c86f56505b9
Joel Fürer <joelfuerer@MacBook-Pro-von-Joel-2.local>
2026-02-08 15:21:56 +0100

    Fix tasks panel height and inner scroll

--- src/App.tsx
+++ src/App.tsx
@@ -895,7 +895,7 @@ export default function App() {
               </Card>
 
               {/* Aufgabenliste rechts */}
-              <Card className="rounded-2xl shadow-sm h-full flex flex-col">
+              <Card className="rounded-2xl shadow-sm h-full lg:sticky lg:top-6 flex flex-col">
                 <CardHeader className="pb-3">
                   <CardTitle className="text-base">Aufgaben am ausgewählten Tag</CardTitle>
                 </CardHeader>
